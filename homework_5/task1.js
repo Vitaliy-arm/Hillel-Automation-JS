061e0189d49a0e06e9df7a2913cc75036e3933bf
Vitalii <vitalii.lohoida@corezoid.com>
2024-12-20 15:06:21 +0200

    Add homework_5

--- homework_5/task1.js
+++ homework_5/task1.js
@@ -1,1 +1,14 @@
+function areaDec (width, height){
+    return width * height;
+};
+console.log(`function declaration - ${areaDec(5, 7)}`);
 
+
+let areaExp = function (width, height){
+    return width * height;
+};
+console.log(`function expression - ${areaExp(6, 3)}`);
+
+
+let areaArrow = (width, height) => width * height;
+console.log(`arrow function - ${areaArrow(7, 4)}`);

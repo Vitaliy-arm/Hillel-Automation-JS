2d1eb61ff2ff3caf4ceb08f5e449272ae8604293
Vitalii <vitalii.lohoida@corezoid.com>
2024-12-23 04:55:02 +0200

    add homework 6

--- homework_6/task3.js
+++ homework_6/task3.js
@@ -1,1 +1,18 @@
+function Divide(numerator, denominator) {
+    // Проверка на числовые значения
+    if (denominator === 0) {
+        throw new Error ('You can\'t divide by zero');
+    } else if (typeof numerator !== 'number' || typeof denominator !== 'number' || isNaN(numerator) || isNaN(denominator)) {
+        throw new Error('The value is not a number');
+    } else {
+        let result = numerator / denominator;
+        console.log(result);
+    }
+}
 
+try {
+    Divide(50, 0);
+
+} catch (error){
+    console.error('Something is wrong...', error.message);
+}

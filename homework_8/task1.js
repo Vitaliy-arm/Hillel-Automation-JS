405e42fdedce8489b80129c3a1f4fd2a363dcf2d
Vitalii <vitalii.lohoida@corezoid.com>
2024-12-31 16:58:40 +0200

    Add homework_8

--- homework_8/task1.js
+++ homework_8/task1.js
@@ -1,1 +1,8 @@
+const student = {};
 
+student.firstName = "Demy";
+student.lastName = "Goldrin";
+student.age = 24;
+student.courses = ["Methematric", "History", "Programing"];
+
+console.log(student);
--- homework_8/task5.js
+++ homework_8/task5.js
@@ -1,1 +1,10 @@
+const users = [
+    {name: "Nikolas", email: "test@mail.com", age: 34},
+    {name: "Robbie", email: "robbie@mail.com", age: 25},
+    {name: "Joan", email: "joan@mail.com", age: 31},
+    {name: "Ted", email: "ted@mail.com", age: 28},
+]
 
+for (const { name, email, age } of users) {
+    console.log(`Name: ${name}, Email: ${email}, Age: ${age}`);
+}

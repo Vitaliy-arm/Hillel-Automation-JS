e45a07451d9dd2385b6cc8c3a890c650fb5ebf74
Vitalii <vitalii.lohoida@corezoid.com>
2025-01-05 22:07:10 +0200

    Add homework 9

--- homework_9/Book.js
+++ homework_9/Book.js
@@ -1,1 +1,60 @@
+export class Book {
+    constructor(title, author, year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
 
+    get title() {
+        return this._title;
+    }
+
+    get author() {
+        return this._author;
+    }
+
+    get year() {
+        return this._year;
+    }
+
+    set title(value) {
+        if (!value) {
+            this._title = "The title field is not specified";
+            return;
+        }
+        this._title = value;
+    }
+
+    set author(value) {
+        if (!value) {
+            this._author = "The author field is not specified";
+            return;
+        }
+        this._author = value;
+    }
+
+    set year(value) {
+        if (!value || isNaN(value) || value < 0) {
+            this._year = "Year must be a valid number!";
+            return;
+        }
+        this._year = value;
+    }
+
+    printInfo() {
+        console.log(`Book: Title - ${this.title}, Author - ${this.author}, Year - ${this.year}`);
+    }
+
+    static getOldestBook(books) {
+        return books.reduce((oldest, current) => {
+            return current.year < oldest.year ? current : oldest;
+        });
+    }
+}
+
+
+
+
+// book1.printInfo();
+// book2.printInfo();
+// book3.printInfo();
--- homework_9/EBook.js
+++ homework_9/EBook.js
@@ -1,1 +1,31 @@
+import {Book} from './Book.js';
 
+export class EBook extends Book {
+    constructor(title, author, year, fileType) {
+        super(title, author, year);
+        this.fileType = fileType;
+    }
+
+    get fileType(){
+        return this._fileType;
+    }
+
+    set fileType(value) {
+        if (!value) {
+            console.log("File type cannot be empty!");
+            this._fileType = "The file type is not specified";
+            return;
+        }
+        this._fileType = value;
+    }
+
+    printInfo() {
+        console.log(`eBook: Title - ${this.title}, Author - ${this.author}, Year - ${this.year}, Type of file - ${this.fileType}`);
+    }
+
+    static createEBookFromBook(book, fileFormat) {
+        return new EBook(book.title, book.author, book.year, fileFormat);
+    }
+}
+
+// eBook1.printInfo();

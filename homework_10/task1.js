abf0ed964b52e94213c630678c5f62b12f2d5595
Vitalii <vitalii.lohoida@corezoid.com>
2025-01-11 13:33:03 +0200

    Add homework_10

--- homework_10/task1.js
+++ homework_10/task1.js
@@ -1,1 +1,5 @@
+function Timer (text, time) {
+    setTimeout(() => console.log(text), time);
+}
 
+Timer("Some text", 3000);
--- homework_10/task2.js
+++ homework_10/task2.js
@@ -1,1 +1,31 @@
+function SendRequestTodo(url1) {
+    return fetch(url1)
+    .then(response => response.json())
+    .then(todo => {
+      return todo;
+    })
+}
 
+function SendRequestUser(url2) {
+    return fetch(url2)
+    .then(response => response.json())
+    .then(user => {
+      return user;
+    })
+}
+
+Promise.all([SendRequestTodo('https://jsonplaceholder.typicode.com/todos/1'), SendRequestUser('https://jsonplaceholder.typicode.com/users/1')])
+.then(results => {
+    console.log('Results from Promise.all:', results);
+})
+.catch(error => {
+    console.error('Error in Promise.all:', error);
+});
+
+Promise.race([SendRequestTodo('https://jsonplaceholder.typicode.com/todos/1'), SendRequestUser('https://jsonplaceholder.typicode.com/users/1')])
+.then(result => {
+    console.log('Result from Promise.race:', result);
+})
+.catch(error => {
+    console.error('Error in Promise.race:', error);
+});
